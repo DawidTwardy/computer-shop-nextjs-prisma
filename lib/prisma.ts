2f0724fc878607ec1239a7bb62fe914b9e1c50eb
DawidTwardy <dawidtwardy21@gmail.com>
2025-12-10 23:47:49 +0100

    Finished Lab13 from BackEnd Programming Authentication

---
+++ lib/prisma.ts
@@ -0,0 +1,25 @@
+// /lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+import "dotenv/config";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const prisma = globalForPrisma.prisma || new PrismaClient({
+  adapter,
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
